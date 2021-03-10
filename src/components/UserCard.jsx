c66ed9cace6bf94c90e9ab11922b2f076795ea3d
Chris Burton <chris@fullstackcaveman.com>
2021-03-09 19:25:48 -0700

    Unused imports cleanup

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import { Grid } from '@material-ui/core';
 import axios from 'axios';
 import Followers from './Followers';
 
